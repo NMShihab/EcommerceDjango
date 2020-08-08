355d9935f9fc55624a9dce8ec773cf61ab146143
NMShihab <nmshihabislam@gmail.com>
2020-08-08 10:03:58 +0600

    Create custom Admin model

--- organi/App_Login/admin.py
+++ organi/App_Login/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from App_Login.models import User, UserProfile
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(UserProfile)
--- organi/organi/settings.py
+++ organi/organi/settings.py
@@ -35,6 +35,9 @@ INSTALLED_APPS = [
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
+# Custom user model
+AUTH_USER_MODEL = 'App_Login.User'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
