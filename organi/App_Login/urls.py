9d95ebf07df90b1a4ebf11635cc9f78cf7135e64
NMShihab <nmshihabislam@gmail.com>
2020-08-09 22:07:00 +0600

    Signup

--- organi/App_Login/urls.py
+++ organi/App_Login/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from App_Login import views
 
 app_name = 'App_Login'
 
 urlpatterns =[
+    path('SignUp/',views.SignUp,name='signup'),
     
 ]
--- organi/App_Login/views.py
+++ organi/App_Login/views.py
@@ -11,5 +11,18 @@ from django.contrib.auth import login,logout,authenticate
 from App_Login.models import User,UserProfile
 from App_Login.forms import ProfileForm, SignUpForm
 
-
 # Create your views here.
+
+def SignUp(request):
+    form = SignUpForm()
+    registered = False
+
+    if request.method == "POST":
+        form = SignUpForm(data = request.POST)
+
+        if form.is_valid():
+            form.save()
+            registered = True
+    
+    return render(request,'App_Login/signup.html',context={'form':form,'registered':registered})
+
