5333541e9d5f7958c2ac2284b930c19cc03c6802
NMShihab <nmshihabislam@gmail.com>
2020-10-14 17:40:04 +0600

    detail page

--- organi/Shop/urls.py
+++ organi/Shop/urls.py
@@ -4,6 +4,7 @@ from Shop import  views
 app_name = 'Shop'
 
 urlpatterns =[
-path('',views.Home.as_view(),name='home')
+path('',views.Home.as_view(),name='home'),
+path('product/<pk>',views.ProductDetail.as_view(),name="detail"),
 
 ]
--- organi/Shop/views.py
+++ organi/Shop/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
+
+#import views
 from django.views.generic import  ListView,DetailView
 
+# import  mixins
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+# import models
 from Shop.models import Product
 
-# Create your views here.
-
+# Home page view
 class Home(ListView):
     model = Product
     template_name = 'Shop/home.html'
+
+
+# Detail page view
+class ProductDetail(LoginRequiredMixin, DetailView):
+    model = Product
+    template_name = "Shop/detail.html"
